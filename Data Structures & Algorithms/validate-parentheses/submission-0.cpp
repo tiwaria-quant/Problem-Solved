248240ac1b4dbd018cb5d5980980af091eea37d1
tiwaria-quant <tiwaria.9929@gmail.com>
2026-06-27 23:18:32 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+       stack <char>st;
+       for(int i = 0;i<s.size();i++){
+        if(st.empty() || s[i]=='{' || s[i]=='[' || s[i]=='('){
+            st.push(s[i]);
+        }
+        else if ((st.top()=='(' && s[i]==')') || (st.top()=='{' && s[i]=='}') ||  (st.top()=='[' && s[i]==']')){
+            st.pop();
+        }
+        else{
+            return false;
+        }
+       } 
+       return st.empty();
+    }
+};
