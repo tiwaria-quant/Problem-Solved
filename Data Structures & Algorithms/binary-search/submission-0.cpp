27af680e35a6bb01ac2dafd22bf9fc0c5fd919ac
tiwaria-quant <tiwaria.9929@gmail.com>
2026-06-28 00:55:22 +0530

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int n = nums.size();
+        int h = n-1;
+        while(l<=h){
+            int m = (l+h)/2;
+            if(nums[m]==target){
+                return m;
+            }
+            else if(nums[m]<target){
+                l=m+1;
+            }
+            else{
+                h=m-1;
+            }
+        }
+        return -1;
+
+    }
+};
