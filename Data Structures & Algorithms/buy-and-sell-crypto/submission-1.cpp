18bb2cb9e45109e5fd4fe586ce743a70d1506194
tiwaria-quant <tiwaria.9929@gmail.com>
2026-07-30 21:54:09 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans = 0;
+        int buy = prices[0];
+        for(int i = 0;i<prices.size();i++){
+            if(prices[i]>buy){
+                int profit = prices[i]-buy;
+                ans = max(ans,profit);
+            }
+            else{
+                buy = prices[i];
+            }
+        }
+        return ans;
+    }
+};
