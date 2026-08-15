168da80e7632b8a691f43dc7e75a182ae9066e84
tiwaria-quant <tiwaria.9929@gmail.com>
2026-08-15 23:02:14 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0;
+        int r = height.size()-1;
+        int leftMax = height[l], rightMax = height[r];
+        int res = 0;
+        while(l<r){
+            if(leftMax<rightMax){
+                l++;
+                leftMax = max(leftMax,height[l]);
+                res+=leftMax-height[l];
+            }
+            else{
+                r--;
+                rightMax = max(rightMax,height[r]);
+                res+=rightMax-height[r];
+            }
+        }
+        return res;
+    }
+};
