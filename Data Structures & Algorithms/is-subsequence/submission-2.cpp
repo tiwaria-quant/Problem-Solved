1ee6bfff44eb6358c88238c199b25ca8c285f7d2
tiwaria-quant <tiwaria.9929@gmail.com>
2026-08-23 22:57:16 +0530

    Add: is-subsequence - submission-2

---
+++ Data Structures & Algorithms/is-subsequence/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j= 0;
+        int i = 0;
+        while(i<s.size() && j<t.size()){
+            if(s[i]==t[j]){
+                i++;
+            }
+            j++;
+        }
+        return (i==(int)s.size());
+    }
+};
